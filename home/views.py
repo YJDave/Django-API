3a1df84cd5e270976384b3b972abfd58ec6df379
YJDave <yashashvidave@gmail.com>
2017-06-14 23:57:06 +0530

    Second Commited

--- home/views.py
+++ home/views.py
@@ -22,48 +22,8 @@ class IPlistAPIView(ListAPIView):
 
     #def get_queryset(self):
 
+#RetrieveAPIView only return one object
+#To retrieve object using primary key
 class IPDetailList(RetrieveAPIView):
     queryset = IPdata.objects.all()
     serializer_class = IPDetailSerializer
-    #lookup_field = 'country'
-
-
-
-"""
-#from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import IPdata
-from .serializers import IPdataSerializer
-from django.http import HttpResponse
-
-from django.db.models import Q
-
-# Create your views here.
-
-class IPlist(APIView):
-
-
-    def get_queryset(self, *args, **kwargs):
-        serializer = IPlist
-        queryset_list = IPdata.objects.filter(user=self.request.user)
-        query = self.request.GET.get("q")
-        if query:
-            queryset_list = queryset_list.filter(
-                Q(country__contains=query)).distinct()
-        return queryset_list
-
-#pass all list of IP
-    def get(self, request):
-
-        all_ip = IPdata.objects.all()
-        serializer = IPdataSerializer(all_ip, many=True)
-        return Response(serializer.data)
-
-    #to add new IP in list or edit it
-    def post(self):
-        pass
-
-"""
